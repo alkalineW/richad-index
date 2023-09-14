0641adc820752d72a1cca179139d0846c02d48a9
alkalineW <dova881@gmail.com>
2023-09-15 00:28:18 +0800

    footer class differed from different page
    pass origin page property to footer component

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './css/Footer.css';
 import './css/Base.css';
 
-function Footer({ year }) {
+function getFullYear() {
+  const dt = new Date();
+  const currentYear = dt.getFullYear();
+  return currentYear;
+}
+
+function Footer({ origin }) {
   return (
-    <p className='Footer_text text-center position-absolute'>
-      {year} ©Lion International
+    <p
+      className={`Footer_text text_center position_absolute ${
+        origin === 'index' ? 'Footer_text_index' : 'Footer_text_about'
+      }`}
+    >
+      {getFullYear()} ©Lion International
     </p>
   );
 }
