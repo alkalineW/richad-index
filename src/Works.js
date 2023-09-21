b4239d653c70afef8ba5ac38dffe702dca3eed42
alkalineW <dova881@gmail.com>
2023-09-21 19:04:34 +0800

    add filter data method & child component of Work page
    filter data method for display works based on categoory

--- src/Works.js
+++ src/Works.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './css/Base.css';
 import './css/Works.css';
 import MenuBar from './MenuBar';
+import WorksGrid from './WorksGrid';
+import imgData from './api/imgList.json';
 
 export default function Works() {
+  const data = imgData.imgList.map((eachImgUrl) => eachImgUrl);
+  console.log(data);
+  const [ workData, setWorkData] = useState(data);
+
+  function filterImgData(data , filterCategory){
+    if (filterCategory === ''){
+      return data;
+    }else{
+      const matchRule = new RegExp( filterCategory, 'gi');
+      const result = data.filter((data)=>data.match(matchRule))
+    return result;
+    }
+  }
+
   return (
     <div className='Works_wrap'>
+      <div>
+      <button onClick={()=>{
+          setWorkData(filterImgData(workData , ''))
+        }}>All</button>
+        <button onClick={()=>{
+          setWorkData(filterImgData(workData , 'packaging'))
+        }}>Packaging</button>
+        <button onClick={()=>{
+          setWorkData(filterImgData(workData , 'web'))
+        }}>Website</button>
+        <button onClick={()=>{
+          setWorkData(filterImgData(workData , 'graphics'))
+        }}>Graphics</button>
+        
+      </div>
       <MenuBar orgin={'Works'} />
-      <p>works</p>
+      <WorksGrid imgList={workData}/>
     </div>
   );
 }
