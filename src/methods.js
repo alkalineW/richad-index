f34fd3cbad856299559e8ec162fe8a06dacbbf41
alkalineW <dova881@gmail.com>
2023-10-01 01:24:00 +0800

    create function that generate sub-arrays from original array
    for generate row of components or sort out array into sub-array

--- src/methods.js
+++ src/methods.js
@@ -5,3 +5,16 @@ export function  importAll(r) {
     });
     return images;
   }
+
+  export function sortOutArray(array, lengthOfArray) {
+    const result = [];
+    if (array.length <= lengthOfArray) {
+      result.push(array);
+    } else {
+      const nuArray = array.slice(0, lengthOfArray);
+      const remainingArray = array.slice(lengthOfArray);
+      result.push(nuArray);
+      result.push(...sortOutArray( remainingArray, lengthOfArray))
+    }
+    return result;
+  }
