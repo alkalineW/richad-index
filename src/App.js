47f7fbb07047010683fb8f16d85e786dbcf06a77
alkalineW <dova881@gmail.com>
2023-10-06 01:32:05 +0800

    add metadata & title for page of index

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './css/App.css';
 import InteractiveIndex from './InteractiveIndex';
 import LoadingScreen from './LoadingScreen';
 import {imgData }from './api/imgList';
+import { Helmet , HelmetProvider} from 'react-helmet-async';
 
 function App() {
   let data = [];
@@ -17,7 +18,18 @@ function App() {
       ))
     : (pageContent = <InteractiveIndex imgList={data} isActive={true} />);
 
-  return <div className='App'>{pageContent}</div>;
+  return (
+    <div className='App'>
+      <HelmetProvider>
+        <Helmet>
+          <title>里安國際</title>
+          <meta name="description" content='里安國際股份有限公司 LiOn international'/>
+          <meta name="keywords" content="平面設計,包裝設計,網頁設計,展場佈置,活動規劃,禮品設計,廣告設計,商業設計"/>
+        </Helmet>
+        {pageContent}
+        </HelmetProvider>
+    </div>
+  );
 }
 
 export default App;
