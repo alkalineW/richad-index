c6f9718a2f6041de11ef2f7365372c205b0cfefb
alkalineW <dova881@gmail.com>
2023-11-22 17:06:01 +0800

    update grid of Work page which route to single project

--- src/Works.js
+++ src/Works.js
@@ -4,6 +4,7 @@ import "./css/Works.css";
 import MenuBar from "./MenuBar";
 import { importAll, sortOutArray } from "./methods";
 import { imgData } from "./api/imgList";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Works() {
   const workImgData = importAll(
@@ -11,6 +12,9 @@ export default function Works() {
   );
   const data = imgData.map((eachData) => eachData);
   const [workData, setWorkData] = useState(data);
+
+  console.log(workData);
+
   // const data = imgData.imgList.map((eachImgUrl) => eachImgUrl);
   // console.log(data);
   // const [ workData, setWorkData] = useState(data);
@@ -30,94 +34,109 @@ export default function Works() {
 
   let workRowArray = generateWorkArray;
 
-  console.log(workRowArray);
-
   const workRow = workRowArray.map((workRowArray) => (
     <div className="grid-row">
-      {workRowArray.map((eachUrl, index) => {
+      {workRowArray.map((eachData, index) => {
+        console.log(eachData);
         let gridElm = "";
+        let workGrid = (
+          <Link
+            to={eachData.category + "/" + eachData.urlName}
+            state={{
+              name: eachData.name,
+              gallery: eachData.gallery,
+            }}
+            key={eachData.id}
+          >
+            <img
+              key={eachData.id}
+              src={eachData.workCover}
+              alt={eachData.name}
+            />
+          </Link>
+        );
         switch ((index + 1) % 12) {
           case 1:
             gridElm = (
-              <div className="grid-1">
-                <img src={eachUrl} />
+              <div className="grid-1" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 2:
             gridElm = (
-              <div className="grid-2">
-                <img src={eachUrl} />
+              <div className="grid-2" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 3:
             gridElm = (
-              <div className="grid-3">
-                <img src={eachUrl} />
+              <div className="grid-3" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 4:
             gridElm = (
-              <div className="grid-4">
-                <img src={eachUrl} />
+              <div className="grid-4" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 5:
             gridElm = (
-              <div className="grid-5">
-                <img src={eachUrl} />
+              <div className="grid-5" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 6:
             gridElm = (
-              <div className="grid-6">
-                <img src={eachUrl} />
+              <div className="grid-6" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 7:
             gridElm = (
-              <div className="grid-7">
-                <img src={eachUrl} />
+              <div className="grid-7" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 8:
             gridElm = (
-              <div className="grid-8">
-                <img src={eachUrl} />
+              <div className="grid-8" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 9:
             gridElm = (
-              <div className="grid-9">
-                <img src={eachUrl} />
+              <div className="grid-9" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 10:
             gridElm = (
-              <div className="grid-10">
-                <img src={eachUrl} />
+              <div className="grid-10" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           case 11:
             gridElm = (
-              <div className="grid-11">
-                <img src={eachUrl} />
+              <div className="grid-11" key={eachData.id}>
+                {workGrid}
               </div>
             );
             break;
           default:
             gridElm = (
-              <div className="grid-12">
-                <img src={eachUrl} />
+              <div className="grid-12" key={eachData.id}>
+                {workGrid}
               </div>
             );
         }
@@ -143,10 +162,11 @@ export default function Works() {
       //   }}>Graphics</button>
         
       // </div> */}
-      <MenuBar origin={"Works"} />
+      <MenuBar origin={"work"} />
       <div className="Works_album_wrap position_relative">
         <div className="grid-outer">{workRow}</div>
       </div>
+      <Outlet />
     </div>
   );
 }
